8cae788a204e60768f6f6b4e44170786633e0987
barioo saro <sarobaridoo@gmail.com>
2023-11-28 15:26:26 -0800

    task 100

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for letter in range(ord('z'), ord('a')-1, -1):
+    if letter % 2 == 0:
+        print('{:c}'.format(letter), end='')
+    else:
+        print(chr(letter-32), end="")
