f830d8f38a6a07c92a7d99562bc7d69dafc844e9
barioo saro <sarobaridoo@gmail.com>
2023-11-28 09:12:13 -0800

    task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in range(ord('a'), ord('z') + 1):
     if chr(letter) != "q" and chr(letter) != "e":
-    print('{:c}'.format(letter), end='')
+        print('{:c}'.format(letter), end='')
