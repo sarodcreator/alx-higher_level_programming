c1f1ba668b44730334bc4a582842d1c449f5f41d
barioo saro <sarobaridoo@gmail.com>
2023-11-28 13:29:32 -0800

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(0, 9):
-    for i in range(1, 10):
+    for i in range((num + 1), 10):
         if num == 8 and i == 9:
             print("{}{}".format(num, i))
         else:
