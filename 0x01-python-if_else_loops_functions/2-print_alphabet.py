b866afad79f664786bcd34797282c410af4a88b7
barioo saro <sarobaridoo@gmail.com>
2023-11-28 09:02:19 -0800

    task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-import string
-for letters in string.ascii_lowercase:
-    print(letters, end = "")
+for letter in range(ord('a'), ord('z') + 1):
+    print('{:c}'.format(letter), end='')
