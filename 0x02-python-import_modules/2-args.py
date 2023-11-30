0548bee85b96c9209dfd1292b88b0bc26559299a
barioo saro <sarobaridoo@gmail.com>
2023-11-30 08:10:37 -0800

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    i = len(sys.argv) - 1
+    if i == 0:
+        print("0 arguments")
+    elif i == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments".format(i))
+    for count in range(i):
+        print("{} : {}".format(count + 1, sys.argv[count + 1]))
