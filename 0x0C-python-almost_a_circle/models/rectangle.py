3b01e1c4dcc77d56b354561c0c074bb6fd9efb64
barioo saro <sarobaridoo@gmail.com>
2024-01-15 14:53:43 -0800

    fixed

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -113,8 +113,8 @@ class Rectangle (Base):
             return
         try:
             self.id = args[0]
-            self.height = args[1]
-            self.width = args[2]
+            self.width = args[1]
+            self.height = args[2]
             self.x = args[3]
             self.y = args[4]
 
