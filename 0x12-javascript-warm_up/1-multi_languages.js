e1864e54260eeef914959cf172b2fa9ab7135c34
Baridoo Saro <sarobaridoo@gmail.com>
2024-03-11 14:42:42 -0700

    task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* A script that prints three lines
+ * @first: C is fun
+ * @second: Python is cool
+ * @third: JavaScript is amazing
+ */
+
+console.log("C is fun\n" + "Python is cool\n" + "JavaScript is amazing")
