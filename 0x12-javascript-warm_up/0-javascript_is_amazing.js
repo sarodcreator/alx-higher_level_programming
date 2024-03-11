1509785bb824c41808d34c66a58fc5879cdee221
Baridoo Saro <sarobaridoo@gmail.com>
2024-03-11 15:10:13 -0700

    fixed

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// A script that prints “JavaScript is amazing”
+// A script that prints “JavaScript is amazingk”
 
 const myVar = "JavaScript is amazing";
-console.log(myVar)
+console.log(myVar);
