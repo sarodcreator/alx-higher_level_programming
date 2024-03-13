280cfc0c523123172dd7f070abc37ef41780b352
Baridoo Saro <sarobaridoo@gmail.com>
2024-03-13 14:32:41 -0700

    task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+class Rectangle
+{
+	constructor (w, h)
+	{
+		if (w > 0 & h > 0)
+		{
+			this.width = w;
+			this.height = h;
+		}
+	}
+
+
+	print()
+	{
+		let i;
+		for (i = 0; i < this.height; i++)
+		{
+			let row = 0;
+			let col = '';
+			for (row; row < this.width; row++)
+			{
+				col += 'X'
+			}
+			console.log(col)
+		}
+	}
+}
+module.exports = Rectangle;
