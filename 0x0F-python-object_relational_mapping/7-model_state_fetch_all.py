359dc0705ccf60cf4ab0142809b3d3876bc0f591
Bridoo Saro <sarobaridoo@gmail.com>
2024-03-16 08:56:38 -0700

    fixed

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,13 +4,13 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from model_state import Base, State
-from sys import argv
+import sys
 
 if __name__ == "__main__":
     """Function that connects to MySQL server on localhost at port 3306"""
 
-    db = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1], argv[2],
-                                                          argv[3])
+    db = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(sys.argv[1], sys.argv[2],
+                                                         sys.argv[3])
     engine = create_engine(db)
     Session = sessionmaker(bind=engine)
     session = Session()
