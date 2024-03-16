40e038ad8cd051548e8ad36415f444e80f60dcbf
Bridoo Saro <sarobaridoo@gmail.com>
2024-03-16 05:11:19 -0700

    fixed

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-"""Script that lists all cities from the database hbtn_0e_4_usa"""
-import MySQLdb
-import sys
+"""
+A script that takes in the name of a state as an argument
+and lists all cities of that state, using the database hbtn_0e_4_usa
+"""
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor()
-    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
-    JOIN states ON cities.state_id = states.id ORDER BY cities.id")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-    cursor.close()
-    db.close()
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+    """Function that connects to MySQL server on localhost at port 3306"""
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    with db.cursor() as cur:
+        cur.execute("SELECT cities.id, cities.name, states.name \
+                     FROM cities \
+                     JOIN states \
+                     ON cities.state_id = states.id \
+                     WHERE states.name LIKE BINARY %(state_name)s \
+                     ORDER BY cities.id ASC", {'state_name': argv[4]})
+        rows = cur.fetchall()
+        if rows is not None:
+            result = ", ".join([row[1] for row in rows])
+            print(result)
