4962c8e1421f4a274ba5d43bf811108a06d830f0
Bridoo Saro <sarobaridoo@gmail.com>
2024-03-16 09:15:13 -0700

    fixed

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-"""Script that lists all State objects that contain the letter a from the
-database hbtn_0e_6_usa"""
-import sys
-from sqlalchemy import create_engine
+"""A script that lists all State objects from the database hbtn_0e_6_usa"""
+
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 from model_state import Base, State
+from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    """Function that connects to MySQL server on localhost at port 3306"""
+
+    db = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1], argv[2],
+            argv[3])
+    engine = create_engine(db)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).filter(State.name.like('%a%'))\
-            .order_by(State.id):
-        print("{}: {}".format(state.id, state.name))
-    session.close()
+    query = session.query(State).filter(State.name.like('%a%'))
+    result = query.all()
+    for row in result:
+        print("{:d}: {:s}".format(row.id, row.name))
