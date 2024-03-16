3da6d8e4227fcc35edb8b600bc7f5bdf60451d48
Bridoo Saro <sarobaridoo@gmail.com>
2024-03-16 08:47:51 -0700

    fixed

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""class definition of a State and an instance Base = declarative_base():
-
-State class:
-inherits from Base Tips
-links to the MySQL table states
-class attribute id that represents a column of an auto-generated, unique integer, can’t be null and is a primary key
-class attribute name that represents a column of a string with maximum 128 characters and can’t be null
+"""
+A python file that contains class definition of State
+and instance Base = declarative_base()
 """
 
-from sys  import argv
-import model_state Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-            (argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.ceate_all(engine)
+Base = declarative_base()
+
+
+class State(Base):
+    """A class that defines State instances"""
+
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
