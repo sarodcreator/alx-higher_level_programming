1a0f7491700aa3cfc91a1469d54332d127cbb804
Bridoo Saro <sarobaridoo@gmail.com>
2024-03-16 04:13:08 -0700

    fixed

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,20 +1,24 @@
-#!/usr/bin/python3
-"""Script that lists all cities from the database hbtn_0e_4_usa"""
-import MySQLdb
-import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor()
-    cursor.execute("SELECT cities.id, cities.name, states.name \
+#!/usr/bin/python3
+"""A script that lists all cities from the database hbtn_0e_4_usa"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+    """Function that connects to MySQL server on localhost at port 3306"""
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    with db.cursor() as cur:
+        cur.execute("SELECT cities.id, cities.name, states.name \
                      FROM cities \
                      JOIN states \
                      ON cities.state_id = states.id \
                      ORDER BY cities.id ASC")
-    rows = cursor.fetchall()
-    if rows is not none:
-        for row in rows:
-            print(row)
-    cursor.close()
-    db.close()
+        rows = cur.fetchall()
+        if rows is not None:
+            for row in rows:
+                print(row)
+
