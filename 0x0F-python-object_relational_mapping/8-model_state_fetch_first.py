df6c27c85e169a097ca3a862776b0ac9e5f58106
Bridoo Saro <sarobaridoo@gmail.com>
2024-03-16 09:07:23 -0700

    fixed

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-"""A script that lists all State objects from the database hbtn_0e_6_usa"""
-
-from sqlalchemy.orm import sessionmaker
+"""Script that prints the first State object from the database
+hbtn_0e_6_usa"""
+import sys
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-from sys import argv
 
 if __name__ == "__main__":
-    """Function that connects to MySQL server on localhost at port 3306"""
-
-    db = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1], argv[2],
-                                                          argv[3])
-    engine = create_engine(db)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    query = session.query(State)
-    result = query.first()
-    if (result):
-        print("{:d}: {:s}".format(result.id, result.name))
+    first = session.query(State).order_by(State.id).first()
+    if first:
+        print("{}: {}".format(first.id, first.name))
     else:
         print("Nothing")
+    session.close()
